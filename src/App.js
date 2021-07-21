d721a2f39c08b69490bfecb6cb31563a2b624075
Атрощенко Константин Анатольевич <asu8@vts.vitebsk.energo.net>
2021-07-21 09:54:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const userData = [
+  { name: 'Jeevan' },
+  { name: 'Manish' },
+  { name: 'Prince' },
+  { name: 'Arti' },
+];
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    setUsers(userData);
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, checked } = e.target;
+    if (name === 'AllSelect') {
+      let tempUser = users.map((user) => {
+        return { ...user, isChecked: checked };
+      });
+      setUsers(tempUser);
+    } else {
+      let tempUser = users.map((user) =>
+        user.name === name ? { ...user, isChecked: checked } : user
+      );
+      setUsers(tempUser);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-4" style={{ width: 400 }}>
+      <form className="form">
+        <h3>Select user</h3>
+        <div className="form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            name="AllSelect"
+            onChange={handleChange}
+            checked={
+              users.filter((user) => user?.isChecked !== true).length < 1
+            }
+          />
+          <label className="form-check-label ms-2">All select</label>
+        </div>
+        {users.map((user) => (
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              name={user.name}
+              onChange={handleChange}
+              checked={user?.isChecked || false}
+            />
+            <label className="form-check-label ms-2">{user.name}</label>
+          </div>
+        ))}
+      </form>
     </div>
   );
 }
